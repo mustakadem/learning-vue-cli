4ea83b6ea7972749fabbaac6dab69f96d4179a70
musta <mustakadem@gmail.com>
2018-04-13 12:20:04 +0200

    primer commit

---
+++ hello-vue/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <persona></persona>
+</template>
+
+<script>
+  import persona from './component/persona'
+export default {
+  components:{persona},
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
