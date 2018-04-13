a9ff6a926cfaace77763c8f58181329ffdebaee1
musta <mustakadem@gmail.com>
2018-04-13 13:09:00 +0200

    comprendiendo hot reload

--- hello-vue/src/App.vue
+++ hello-vue/src/App.vue
@@ -1,8 +1,15 @@
 <template>
   <div>
-
+      <h2>Componente persona</h2>
     <persona></persona>
+    <hr>
+    <h2>Componente elemento</h2>
+
     <elemento tipo="footer">Hola mundo</elemento>
+    <hr>
+    <h2>Componente tarea</h2>
+
+    <tareas></tareas>
 
   </div>
 
@@ -11,8 +18,9 @@
 <script>
   import persona from './component/persona'
   import elemento from './component/elemento'
+  import tareas from './component/tareas'
 export default {
-  components:{persona,elemento},
+  components:{persona,elemento,tareas},
   data () {
     return {
 
---
+++ hello-vue/src/component/tareas.vue
@@ -0,0 +1,54 @@
+<template>
+
+  <div>
+    <h2>Tareas por hacer</h2>
+
+    <ul>
+      <li v-for="tarea in tareasPendientes">
+        <a href="#" @click="actualizarTarea(tarea)" v-text="tarea.nombre"></a>
+      </li>
+    </ul>
+    <h2>Las tareas finalizadas</h2>
+    <li v-for="tarea in tareasFinalizadas">
+      <a href="#" @click="actualizarTarea(tarea)" v-text="tarea.nombre"></a>
+    </li>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    data(){
+      return {
+        tareas: [
+          {nombre: 'Aprender vue.js',completado:false},
+          {nombre: 'Grabar video vue.js',completado:false},
+          {nombre: 'Leer doc vue.js',completado:false},
+          {nombre: 'Salir a ver el sol con vue.js',completado:false},
+        ],
+        finalizadas: []
+      }
+    },
+    methods: {
+       actualizarTarea(tarea){
+         tarea.completado = !tarea.completado
+       }
+    },
+    computed: {
+      tareasPendientes(){
+        return this.tareas.filter((tarea) => !tarea.completado);
+      },
+      tareasFinalizadas(){
+        return this.tareas.filter((tarea) => tarea.completado);
+      }
+    }
+  }
+</script>
+
+<style>
+  li a{
+    color:darkblue;
+    font-weight: bold;
+    text-decoration: none;
+  }
+</style>
